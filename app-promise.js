0141d99b23f7cfd071d8a3b509e99a3ed27dc76e
AndresHaayen <andreshaayen@gmail.com>
2017-10-25 07:38:31 +0200

    Followed the course and updated the subject

--- app-promise.js
+++ app-promise.js
@@ -15,13 +15,27 @@ const argv = yargs
     .argv;
     
 var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = `https://mapsgoogleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) => {
-    console.log(response.data);
+    if(response.data.status === 'ZERO_RESULTS'){
+        throw new Error('Unable to find that address');
+    }
+
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/39e2d666673cdc408034539f714f83b1/${lat}, ${lng}`;
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+}).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}`);
 }).catch((e) => {
     if(e.code === 'ENOTFOUND'){
         console.log('Unable to connect to API servers');
+    } else {
+        console.log(e.message);
     }
 });
 
